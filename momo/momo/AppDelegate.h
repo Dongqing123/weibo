a9372936c0c295936ae5f9d41f8a80b8ec2c505f
Dongqing123 <705882260@qq.com>
2018-04-11 09:35:03 +0800

    初如化项目

---
+++ momo/momo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  momo
+//
+//  Created by YZ on 2018/4/11.
+//  Copyright © 2018年 YZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
